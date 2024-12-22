76dc66c4c9d6110c9f695b85bfa1ded172d861a6
OlePundit <glennomondi@yahoo.com>
2024-12-22 06:30:22 +0300

    swipper

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -848,7 +848,6 @@
         delay: 600,
         disableOnInteraction: false,
     },
-    slidesPerView: 4,
     spaceBetween: 40, // Adjust spacing
     breakpoints: {
         575:{
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -181,7 +181,49 @@ $(document).ready(function () {
 });
 
 </script>
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+<script>
+  document.addEventListener('DOMContentLoaded', function () {
+    var swiper2 = new Swiper('.swiper1', {
+      loop:true,
+      pagination: {
+          el: '.swiper-pagination1',
+          clickable: true,
+      },
 
+      navigation: {
+          nextEl: '.swiper-button-next1',
+          prevEl: '.swiper-button-prev1',
+      },
+
+      autoplay: {
+          delay: 600,
+          disableOnInteraction: false,
+      },
+      spaceBetween: 40, // Adjust spacing
+      breakpoints: {
+          575:{
+              slidesPerView:1,
+              spaceBetween:10,
+          },
+          640: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+          },
+          768: {
+              slidesPerView: 3,
+              spaceBetween: 30,
+          },
+          992: {
+              slidesPerView:4,
+              spaceBetween:40,
+          },
+      },
+      speed: 600,
+  });
+});
+
+</script>
   <script>
   new WOW().init();
   </script>
