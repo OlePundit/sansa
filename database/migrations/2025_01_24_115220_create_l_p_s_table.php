3d0e2711a02f16cc2a45a060779df4fb4967e42e
OlePundit <glennomondi@yahoo.com>
2025-01-24 15:21:57 +0300

    dynamic lps

--- database/migrations/2025_01_24_115220_create_l_p_s_table.php
+++ database/migrations/2025_01_24_115220_create_l_p_s_table.php
@@ -18,6 +18,18 @@ return new class extends Migration
             $table->string('title2')->nullable();
             $table->string('title3')->nullable();
             $table->string('img1')->nullable();
+            $table->string('img2')->nullable();
+            $table->string('img3')->nullable();
+            $table->text('faq1')->nullable();
+            $table->text('faq2')->nullable();
+            $table->text('faq3')->nullable();
+            $table->text('faq4')->nullable();
+            $table->text('faq5')->nullable();
+            $table->text('ans1')->nullable();
+            $table->text('ans2')->nullable();
+            $table->text('ans3')->nullable();
+            $table->text('ans4')->nullable();
+            $table->text('ans5')->nullable();
             $table->text('benefits')->nullable();
             $table->text('benefits2')->nullable();
             $table->timestamps();
