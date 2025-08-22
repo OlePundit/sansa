e0f8dfa0536160d2f19392fd3559629dac197f67
OlePundit <glennomondi@yahoo.com>
2025-08-22 11:59:35 +0300

    chenaniah icon

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -387,7 +387,7 @@
 
                                     </div>
                                     <div class="swiper-slide">
-                                        <img src="/storage/chenaniah-logo.webp" alt="logo" width="100px">
+                                        <img src="/storage/chenaniah-logo.webp" width="200" alt="logo" width="100px">
                                     </div>
                                     <div class="swiper-slide">
                                         <img src="/storage/ictalogo.webp" alt="logo" width="100px">
