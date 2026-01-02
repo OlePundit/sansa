8907012da22db50f0595c0ee7adae3970895373a
OlePundit <glennomondi@yahoo.com>
2026-01-02 20:36:26 +0300

    resolving merge conflicts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -13,19 +12,3 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
-=======
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:8000/api/:path*',
-      },
-    ];
-  },
-};
-
-export default nextConfig;
->>>>>>> eaa606a95c18e97417951e5d3112476be20bc2c1
