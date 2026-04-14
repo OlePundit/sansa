10f44cad16dd11d320e473e1ccdbdbe7fb5eb7dd
OlePundit <glennomondi@yahoo.com>
2026-04-14 18:59:57 +0300

    google tag manager

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import { GoogleTagManager } from '@next/third-parties/google';
+import Script from 'next/script';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -16,9 +16,36 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head />
+      <body>
+        {children}
+        
+        {/* GTM noscript fallback - only for users with JS disabled */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+      </body>
+      
+      {/* GTM script - only in production and if gtmId exists */}
       {process.env.NODE_ENV === 'production' && gtmId && (
-        <GoogleTagManager gtmId={gtmId} />
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${gtmId}');
+            `,
+          }}
+        />
       )}
     </html>
   );
